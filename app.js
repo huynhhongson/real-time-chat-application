1120e6ab0b1792fb0298e6cb7f6800d7cd62cf2d
huynhhongson <hhongson5011@gmail.com>
2025-04-14 15:56:44 +0700

    start my project

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT =  process.env.PORT || 3000
+const sever = app.listen(PORT, () => console.log(`server on port ${PORT}`))
+
+app.use(express.static(path.join(__dirname, 'public')))
